0ab0a6358c402abd8cabba861ce85052b38e9f8e
Rikowet <konyushencko.dima@yandex.ru>
2025-03-16 20:07:13 +0300

    Hi

--- english_bot.py
+++ english_bot.py
@@ -23,39 +23,6 @@ user_sessions = {}
 moscow_timezone = 'Europe/Moscow'
 scheduler = AsyncIOScheduler()
 
-beginner_words = [('air', 'воздух'),
-  ('earth', 'земля'),
-  ('light', 'свет'),
-  ('shadow', 'тень'),
-  ('flower', 'цветок'),
-  ('sound', 'звук'),
-  ('sun', 'солнце'),
-  ('moon', 'луна'),
-  ('mirror', 'зеркало'),
-  ('tree', 'дерево')]
-
-moderate_words = [('adventure', 'приключение'),
-  ('harmony', 'гармония'),
-  ('illuminate', 'освещать'),
-  ('jubilant', 'восторженный'),
-  ('meticulous', 'тщательный'),
-  ('nurture', 'воспитывать'),
-  ('optimistic', 'оптимистичный'),
-  ('resilient', 'устойчивый, стойкий'),
-  ('serenity', 'безмятежность'),
-  ('vibrant', 'живой, яркий')]
-
-advanced_words = [('magnanimous', 'великодушный, благородный'),
-  ('disparate', 'неравный, несопоставимый'),
-  ('ineffable', 'неописуемый, несказанный'),
-  ('recalcitrant', 'упрямый, непокорный'),
-  ('perspicacious', 'проницательный, ясно видящий'),
-  ('ephemeral', 'мимолетный, недолговечный'),
-  ('erudite', 'эрудированный, образованный'),
-  ('quotidian', 'повседневный, обыденный'),
-  ('mellifluous', 'сладкозвучный'),
-  ('obfuscate', 'затуманивать, усложнять')]
-
 
 all_level_words = beginner_words + moderate_words + advanced_words
 
